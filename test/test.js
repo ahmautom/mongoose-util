76310f9d093098f424c87e19c93b6ddb3983afd8
Tom Mau <Tom.Mau@dowjones.com>
2014-12-08 18:00:53 +0800

    working

--- test/test.js
+++ test/test.js
@@ -3,7 +3,14 @@ var expect = chai.expect;
 var mongooseUtil = require('../lib');
 
 describe('mongooseUtil.toMongooseSchema', function() {
-    it('should not support array type', function() {
+    it('should transform json schema into mongoose compatible schema object (draft-04)', function() {
+        var obj = mongooseUtil.toMongooseSchema(json);
+        expect(obj).to.eql({
+
+        });
+    });
+
+    it('should transform support array type', function() {
         expect(function() {
             mongooseUtil.toMongooseSchema();
         }).to.throw();
